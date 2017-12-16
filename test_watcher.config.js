3a6d9facb2c869ae1f7d95c0a828fcf1a7a4b3c3
William Rusnack <williamrusnack@gmail.com>
2017-12-16 08:57:10 -0500

    test_watcher now uses a config file

---
+++ test_watcher.config.js
@@ -0,0 +1,33 @@
+const { exec } = require('child-process-promise')
+
+module.exports = {
+  apply_validation: {
+    build() {return exec('npx browserify -e apply_validation-entry.js -o apply_validation-bundle.js')},
+    test_file: 'test/test_apply_validation.js',
+    to_test: 'apply_validation.js',
+  },
+  while_monitoring: {
+    test_file: 'test/test_while_monitoring.js',
+    to_test: 'test/while_monitoring.js',
+  },
+  create_form_routes: {
+    test_file: 'test/test_create_form_routes.js',
+    to_test: 'create_form_routes.js',
+    extra_dependancies: [
+      'apply_validation-entry.js',
+      'apply_validation-bundle.js',
+    ],
+  },
+  form_builder: {
+    test_file: 'test/test_form_builder.js',
+    to_test: 'form_builder.js',
+  },
+  add_method: {
+    test_file: 'test/test_add_method.js',
+    to_test: 'add_method.js',
+  },
+  support_test_functions: {
+    test_file: 'test/test_support_test_functions.js',
+    to_test: 'test/support_test_functions.js',
+  },
+}
--- test_watcher.js
+++ test_watcher.js
@@ -12,41 +12,11 @@ const {dependency_tree, sort_test_order} = require('./dependency_tree.js')
 
 const _ = require('lodash')
 
-
-const config = {
-  apply_validation: {
-    build() {return exec('npx browserify -e apply_validation-entry.js -o apply_validation-bundle.js')},
-    test_file: 'test/test_apply_validation.js',
-    to_test: 'apply_validation.js',
-  },
-  while_monitoring: {
-    test_file: 'test/test_while_monitoring.js',
-    to_test: 'test/while_monitoring.js',
-  },
-  create_form_routes: {
-    test_file: 'test/test_create_form_routes.js',
-    to_test: 'create_form_routes.js',
-    extra_dependancies: [
-      'apply_validation-entry.js',
-      'apply_validation-bundle.js',
-    ],
-  },
-  form_builder: {
-    test_file: 'test/test_form_builder.js',
-    to_test: 'form_builder.js',
-  },
-  add_method: {
-    test_file: 'test/test_add_method.js',
-    to_test: 'add_method.js',
-  },
-  support_test_functions: {
-    test_file: 'test/test_support_test_functions.js',
-    to_test: 'test/support_test_functions.js',
-  },
-}
+const config = require('./test_watcher.config.js')
 
 
 setup(config)
+
 
 
 async function setup(config, queue_time=300) {
